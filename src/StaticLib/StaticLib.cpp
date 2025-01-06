189328ed473b868ee5c70177f755323446dfda26
Notherthing95 <flyingconsole@icloud.com>
2025-01-07 03:22:02 +0900

    implement bubble sort

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,25 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+    if (begin == nullptr || end == nullptr || begin >= end) {   //nullptrの方が良いとありました
+        return false;
+    }
+
+    size_t size = end - begin;  //要素数
+    if (size == 1) {
+        return true;
+    }
+
+    for (size_t i = 0; i < size - 1; i++) {
+        for (size_t j = 0; j < size - 1 - i; j++) {
+            if (begin[j].key > begin[j + 1].key) {
+                // 要素を入れ替える
+                item tmp = begin[j];
+                begin[j] = begin[j + 1];
+                begin[j + 1] = tmp;
+            }
+        }
+    }
+    
+    return true;
 }
